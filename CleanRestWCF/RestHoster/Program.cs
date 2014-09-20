8f7a96c6324358d2fc3bbf1b282ff81ea8be1f4f
CIJAIZ <cijai4u@gmail.com>
2014-09-20 15:29:23 +0530

    Added Clean REST Service Source
    Native Checkin

---
+++ CleanRestWCF/RestHoster/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel.Web;
+using CleanRestWCF;
+
+namespace RestHoster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebServiceHost host = new WebServiceHost(typeof(Service1)))
+            {
+                host.Open();
+                Console.WriteLine("Service is up and Running at {0}... \n Press any key to shut the service down..", host.BaseAddresses[0].AbsoluteUri);
+                Console.ReadKey();
+                host.Close();
+            }
+
+            Console.WriteLine("Service has shut down..");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CleanRestWCF/TestBench/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestBench
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:20498/Service1.svc/getxml/1");
+            request.ContentType = "text/json";
+            request.Method = "GET";
+            string responseText = string.Empty;
+
+            //using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+            //{
+            //    streamWriter.Write("1");
+            //}
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            using (var streamReader = new StreamReader(response.GetResponseStream()))
+            {
+                responseText = streamReader.ReadToEnd();
+            }
+
+            Console.Write("Response returned from the Service is {0}...", responseText);
+            Console.ReadKey();
+        }
+    }
+}
